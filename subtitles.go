a4e73ad735b2f0fe8bf0d2e1d7272cee2fc428a9
Andrey Sokolov <falconandy@yandex.ru>
2019-02-08 11:30:00 +0400

    Subtitles HTML cleaner

--- subtitles.go
+++ subtitles.go
@@ -1,6 +1,7 @@
 package langlearn
 
 import (
+	"regexp"
 	"time"
 )
 
@@ -10,4 +11,18 @@ type Subtitle struct {
 	Start time.Duration
 	End   time.Duration
 	Text  []string
+}
+
+type htmlMarkupCleaner struct {
+	tagRe *regexp.Regexp
+}
+
+func newHTMLMarkupCleaner() *htmlMarkupCleaner {
+	return &htmlMarkupCleaner{
+		tagRe: regexp.MustCompile(`[<{]\s*/?\s*(b|u|i|font)\b[^>}]*[>}]`),
+	}
+}
+
+func (c *htmlMarkupCleaner) Clean(subtitleLine string) string {
+	return c.tagRe.ReplaceAllLiteralString(subtitleLine, "")
 }
---
+++ subtitles_test.go
@@ -0,0 +1,19 @@
+package langlearn
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTMLMarkupCleaner(t *testing.T) {
+	c := newHTMLMarkupCleaner()
+
+	assert.Equal(t, "", c.Clean(""))
+	assert.Equal(t, "test", c.Clean("test"))
+	assert.Equal(t, "test", c.Clean("<b>test"))
+	assert.Equal(t, "test", c.Clean("test</b>"))
+	assert.Equal(t, "test", c.Clean("te{i}st"))
+	assert.Equal(t, "test", c.Clean("tes{/i}t"))
+	assert.Equal(t, "test", c.Clean("<font color='red'>te</font>st"))
+}
