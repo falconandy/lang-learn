321a65495e1b567ef4802e06d355282142af5550
Andrey Sokolov <falconandy@yandex.ru>
2019-02-08 20:28:18 +0400

    Frontend structure with TypeScript support

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  public render() {
+    return (
+      <div className="App">
+        Learn the Language!
+      </div>
+    );
+  }
+}
+
+export default App;
